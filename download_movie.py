acff37a932e48f6c0a76cd786b43a4d368c9de7a
Bader alotaibi <baderalotaibi3@gmail.com>
2024-04-10 23:00:52 +0300

    fix

--- download_movie.py
+++ download_movie.py
@@ -14,9 +14,31 @@ def getallmovies():
             movies.append(file)
     return movies
 
+def fonud_similiraty(movie1,movie2):
+    similarity = 0
+    for i in range(len(movie1)):
+        if movie1[i] == movie2[i]:
+            similarity += 1
+    return True if similarity/len(movie1) > 0.8 else False
+
+def is_dublicated(movies):
+    #search for dublicated movies in os.listdir("movies") with 80% similarity
+    dublicated_movies = []
+    for movie in movies:
+        for file in os.listdir("movies"):
+            if fonud_similiraty(movie[:-4],file[:-4]) in file:
+                dublicated_movies.append(movie)
+    for movie in dublicated_movies:
+        movies.remove(movie)
+    return movies
+    
 
 
-print(getallmovies())
-for movie in getallmovies():
+list_=getallmovies()
+
+list_ = is_dublicated(list_)
+
+for movie in list_:
+    print(f"Downloading {movie}")
     download_movie(movie)
     sleep(5)
