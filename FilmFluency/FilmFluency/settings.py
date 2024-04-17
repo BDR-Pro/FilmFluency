8161303298fefea458c75d5aa9ad31a4abd75d10
Bader alotaibi <baderalotaibi3@gmail.com>
2024-04-17 14:55:46 +0300

    fix db

--- FilmFluency/FilmFluency/settings.py
+++ FilmFluency/FilmFluency/settings.py
@@ -96,6 +96,7 @@ if os.getenv('ENV') == 'development':
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+            'password': 'password',
         }
     }
 else:
