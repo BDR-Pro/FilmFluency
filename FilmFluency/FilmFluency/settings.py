dd426191e99fca781d40caf3766d7ca827329916
Bader alotaibi <baderalotaibi3@gmail.com>
2024-04-19 23:05:01 +0300

    fixed

--- FilmFluency/FilmFluency/settings.py
+++ FilmFluency/FilmFluency/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
     'users',
@@ -191,10 +190,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
