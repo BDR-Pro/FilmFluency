86fcfccb1a652bd770a4c8b469b9ed5b6a5e2ee8
Bader alotaibi <baderalotaibi3@gmail.com>
2024-04-22 18:33:02 +0300

    debug

--- FilmFluency/FilmFluency/settings.py
+++ FilmFluency/FilmFluency/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('django')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['filmfluency.com', 'www.filmfluency.com', 'localhost']
 
